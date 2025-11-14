e970fb4d7bc6c7a3a2e1f5fa8c1865e354f2ee2b
choonewza-pro <choonewza@gmail.com>
2025-11-14 11:57:41 +0700

    test 1

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   // Use repository subpath only for production builds. Keeps local dev/preview working at '/'.
-  base: process.env.NODE_ENV === 'production' ? '/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/my-knowledge-app' : '/',
   plugins: [tailwindcss(), react()],
 });
